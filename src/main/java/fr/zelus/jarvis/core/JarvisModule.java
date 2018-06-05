4507ff8ffbce0575e9d93d70bef7a0db2f362c6e
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-06-05 23:16:13 +0200

    Fix JarvisModule javadoc

--- src/main/java/fr/zelus/jarvis/core/JarvisModule.java
+++ src/main/java/fr/zelus/jarvis/core/JarvisModule.java
@@ -1,17 +1,16 @@
 package fr.zelus.jarvis.core;
 
 import com.google.cloud.dialogflow.v2.Intent;
-import fr.zelus.jarvis.spotify.SpotifyModule;
 
 /**
  * Represents a set of semantically related functions that accepts {@link Intent}s and create {@link JarvisAction}s
  * from them.
  * <p>
- * {@link JarvisModule}s are used to represent action libraries, for example a set of Spotify-related actions (see
- * {@link SpotifyModule}). Handling an {@link Intent} consist of processing its
+ * {@link JarvisModule}s are used to represent action libraries, for example a set of chat-related actions (such as
+ * <i>postMessage</i>, <i>postComment</i>, <i>reply</i> actions). Handling an {@link Intent} consist of processing its
  * content (context variables, previous {@link Intent}, etc) and returning a {@link JarvisAction} representing the
- * action to perform. The returned {@link JarvisAction} is then processed by the
- * {@link JarvisCore} component, that orchestrate the different {@link JarvisModule}s.
+ * action to perform. The returned {@link JarvisAction} is then processed by the {@link JarvisCore} component, that
+ * orchestrate the different {@link JarvisModule}s.
  *
  * @see JarvisCore
  * @see JarvisAction
