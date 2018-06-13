3d452651b2a2ae9635f9dd7779b11335bfe23d90
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-06-13 11:32:54 +0200

    add ModuleCreatorUtil javadoc
    This commit also moves the LogModule.xmi model from /resources/ to /resources/modules/

--- core/src/test/java/fr/zelus/jarvis/util/ModuleCreatorUtil.java
+++ core/src/test/java/fr/zelus/jarvis/util/ModuleCreatorUtil.java
@@ -1,5 +1,6 @@
 package fr.zelus.jarvis.util;
 
+import fr.inria.atlanmod.commons.log.Log;
 import fr.zelus.jarvis.module.Action;
 import fr.zelus.jarvis.module.Module;
 import fr.zelus.jarvis.module.ModuleFactory;
@@ -10,11 +11,27 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
+import java.io.IOException;
 import java.util.Collections;
 
+/**
+ * An utility class that creates {@link Resource} instances representing jarvis core {@link Module}s.
+ * <p>
+ * This class is used to create instances of the models that are stored under the {@code resources/modules} folder,
+ * and can be used in tests to easily retrieve loaded instances of these models.
+ */
 public class ModuleCreatorUtil {
 
-    public static Resource createLogModule() throws Exception {
+    /**
+     * Creates an instance of the {@link fr.zelus.jarvis.core.module.log.LogModule} model
+     * <p>
+     * This method attempts to save the created model in the {@code /tmp/jarvis/test} folder. If the model can not be
+     * saved (because of permission restrictions or invalid path) the in-memory {@link Resource} is returned and a
+     * warning log message is displayed.
+     *
+     * @return an instance of the {@link fr.zelus.jarvis.core.module.log.LogModule} model
+     */
+    public static Resource createLogModule() {
         ModuleFactory moduleFactory = ModuleFactory.eINSTANCE;
         Module module = moduleFactory.createModule();
         module.setName("Log");
@@ -46,10 +63,15 @@ public class ModuleCreatorUtil {
 
         ResourceSet rSet = new ResourceSetImpl();
         rSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-        Resource resource = rSet.createResource(URI.createURI("/tmp/test/logModule.xmi"));
+        Resource resource = rSet.createResource(URI.createURI("/tmp/jarvis/test/logModule.xmi"));
         resource.getContents().clear();
         resource.getContents().add(module);
-        resource.save(Collections.emptyMap());
+        try {
+            resource.save(Collections.emptyMap());
+        } catch (IOException e) {
+            Log.warn("Cannot save the LogModule resource (uri={0}), returning an in-memory instance", resource.getURI
+                    ());
+        }
         return resource;
     }
 }
