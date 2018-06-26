00f0a61c74066cc77e23b1f2bbdc2189ca18a468
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-06-26 16:41:17 +0200

    DialogFlowApi now provides methods to register/delete/get intents
    This allows to automatically build the bot agent from the provided OrchestrationModel, and limit client configuration to its minimum.

--- core/src/main/java/fr/zelus/jarvis/dialogflow/DialogFlowApi.java
+++ core/src/main/java/fr/zelus/jarvis/dialogflow/DialogFlowApi.java
@@ -1,6 +1,10 @@
 package fr.zelus.jarvis.dialogflow;
 
+import com.google.api.core.ApiFuture;
+import com.google.api.gax.rpc.FailedPreconditionException;
+import com.google.cloud.ProjectName;
 import com.google.cloud.dialogflow.v2.*;
+import com.google.longrunning.Operation;
 import fr.inria.atlanmod.commons.log.Log;
 import fr.zelus.jarvis.core.JarvisCore;
 import fr.zelus.jarvis.intent.IntentDefinition;
@@ -9,8 +13,11 @@ import fr.zelus.jarvis.intent.RecognizedIntent;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 import static fr.inria.atlanmod.commons.Preconditions.checkArgument;
 import static fr.inria.atlanmod.commons.Preconditions.checkNotNull;
@@ -19,7 +26,7 @@ import static java.util.Objects.nonNull;
 
 /**
  * A wrapper of the DialogFlow API that provides utility methods to connect to a given DialogFlow project, start
- * sessions, and detect intents from textual inputs.
+ * sessions, manage registered intents, and detect intents instances from textual inputs.
  * <p>
  * This class is used to easily setup a connection to a given DialogFlow project. Note that in addition to the
  * constructor parameters, the {@code GOOGLE_APPLICATION_CREDENTIALS} environment variable must be set and point to
@@ -45,12 +52,55 @@ public class DialogFlowApi {
     private String languageCode;
 
     /**
+     * Represents the DialogFlow project name.
+     * <p>
+     * This attribute is used to compute project-level operations, such as the training of the underlying
+     * DialogFlow's agent.
+     *
+     * @see #trainMLEngine()
+     */
+    private ProjectName projectName;
+
+    /**
+     * Represents the DialogFlow agent name.
+     * <p>
+     * This attribute is used to compute intent-level operations, such as retrieving the list of registered
+     * {@link Intent}s, or deleting specific {@link Intent}s.
+     *
+     * @see #registerIntentDefinition(IntentDefinition)
+     * @see #deleteIntentDefinition(IntentDefinition)
+     */
+    private ProjectAgentName projectAgentName;
+
+    /**
+     * The client instance managing DialogFlow agent-related queries.
+     * <p>
+     * This client is used to compute project-level operations, such as the training of the underlying DialogFlow's
+     * agent.
+     *
+     * @see #trainMLEngine()
+     */
+    private AgentsClient agentsClient;
+
+    /**
+     * The client instance managing DialogFlow intent-related queries.
+     * <p>
+     * This client is used to compute intent-level operations, such as retrieving the list of registered
+     * {@link Intent}s, or deleting specific {@link Intent}s.
+     *
+     * @see #registerIntentDefinition(IntentDefinition)
+     * @see #deleteIntentDefinition(IntentDefinition)
+     */
+    private IntentsClient intentsClient;
+
+    /**
      * The client instance managing DialogFlow sessions.
      * <p>
      * This instance is used to initiate new sessions (see {@link #createSession()}) and send {@link Intent}
      * detection queries to the DialogFlow engine.
      */
     private SessionsClient sessionsClient;
+
 
     /**
      * The {@link IntentFactory} used to create {@link RecognizedIntent} instances from DialogFlow computed
@@ -93,6 +143,10 @@ public class DialogFlowApi {
             this.projectId = projectId;
             this.languageCode = languageCode;
             this.sessionsClient = SessionsClient.create();
+            this.intentsClient = IntentsClient.create();
+            this.projectAgentName = ProjectAgentName.of(projectId);
+            this.agentsClient = AgentsClient.create();
+            this.projectName = ProjectName.of(projectId);
             this.intentFactory = IntentFactory.eINSTANCE;
         } catch (IOException e) {
             throw new DialogFlowException("Cannot construct the DialogFlow API", e);
@@ -118,6 +172,165 @@ public class DialogFlowApi {
     }
 
     /**
+     * Returns the full descriptions of the {@link Intent}s that are registered in the DialogFlow project.
+     * <p>
+     * The full descriptions of the {@link Intent}s include the {@code training phrases}, that are typically used in
+     * testing methods to check that a created {@link Intent} contains all the information provided to the API. To
+     * get a partial description of the registered {@link Intent}s see {@link #getRegisteredIntents()}.
+     * <p>
+     * <b>Note:</b> this method is protected for testing purposes, and should not be called by client code.
+     *
+     * @return the full descriptions of the {@link Intent}s that are registered in the DialogFlow project
+     * @throws DialogFlowException if the {@link DialogFlowApi} is shutdown
+     */
+    protected List<Intent> getRegisteredIntentsFullView() {
+        if (isShutdown()) {
+            throw new DialogFlowException("Cannot retrieve the registered Intents (full view), the DialogFlow API is " +
+                    "shutdown");
+        }
+        List<Intent> registeredIntents = new ArrayList<>();
+        ListIntentsRequest request = ListIntentsRequest.newBuilder().setIntentView(IntentView.INTENT_VIEW_FULL)
+                .setParent(projectAgentName.toString()).build();
+        for (Intent intent : intentsClient.listIntents(request).iterateAll()) {
+            registeredIntents.add(intent);
+        }
+        return registeredIntents;
+    }
+
+    /**
+     * Returns the partial description of the {@link Intent}s that are registered in the DialogFlow project.
+     * <p>
+     * The partial descriptions of the {@link Intent}s does not include the {@code training phrases}. To get a full
+     * description of the registered {@link Intent}s see {@link #getRegisteredIntentsFullView()}
+     * <p>
+     * <b>Note:</b> this method is protected for testing purposes, and should not be called by client code.
+     *
+     * @return the partial descriptions of the {@link Intent}s that are registered in the DialogFlow project
+     * @throws DialogFlowException if the {@link DialogFlowApi} is shutdown
+     */
+    protected List<Intent> getRegisteredIntents() {
+        if (isShutdown()) {
+            throw new DialogFlowException("Cannot retrieve the registered Intents (partial view), the DialogFlow API " +
+                    "is shutdown");
+        }
+        List<Intent> registeredIntents = new ArrayList<>();
+        for (Intent intent : intentsClient.listIntents(projectAgentName).iterateAll()) {
+            registeredIntents.add(intent);
+        }
+        return registeredIntents;
+    }
+
+    /**
+     * Registers the provided {@code intentDefinition} in the DialogFlow project.
+     * <p>
+     * This method reuses the information contained in the provided {@link IntentDefinition} to create a new
+     * DialogFlow {@link Intent} and add it to the current project.
+     * <p>
+     * <b>Note:</b> this method does not train the underlying DialogFlow Machine Learning Engine, so multiple calls
+     * to this method are not generating multiple training calls. Once all the {@link IntentDefinition}s have been
+     * registered to the DialogFlow project use {@link #trainMLEngine()} to train the ML Engine.
+     *
+     * @param intentDefinition the {@link IntentDefinition} to register to the DialogFlow project
+     * @throws DialogFlowException if the {@link DialogFlowApi} is shutdown, or if the {@link Intent} already exists in
+     *                             the DialogFlow project
+     * @see #trainMLEngine()
+     */
+    public void registerIntentDefinition(IntentDefinition intentDefinition) {
+        if (isShutdown()) {
+            throw new DialogFlowException(MessageFormat.format("Cannot register the Intent {0}, the DialogFlow API is" +
+                    " " +
+                    "shutdown", intentDefinition.getName()));
+        }
+        checkNotNull(intentDefinition, "Cannot register the IntentDefinition null");
+        checkNotNull(intentDefinition.getName(), "Cannot register the IntentDefinition with null as its name");
+        Log.info("Registering intent {0}", intentDefinition.getName());
+        List<String> trainingSentences = intentDefinition.getTrainingSentences();
+        List<Intent.TrainingPhrase> dialogFlowTrainingPhrases = new ArrayList<>();
+        for (String trainingSentence : trainingSentences) {
+            dialogFlowTrainingPhrases.add(Intent.TrainingPhrase.newBuilder().addParts(Intent.TrainingPhrase.Part
+                    .newBuilder().setText(trainingSentence).build()).build());
+        }
+        Intent intent = Intent.newBuilder().setDisplayName(intentDefinition.getName()).addAllTrainingPhrases
+                (dialogFlowTrainingPhrases).build();
+        try {
+            Intent response = intentsClient.createIntent(projectAgentName, intent);
+            Log.info("Intent {0} successfully registered", response.getDisplayName());
+        } catch (FailedPreconditionException e) {
+            if (e.getMessage().contains("already exists")) {
+                String errorMessage = MessageFormat.format("Cannot register the intent {0}, the intent already " +
+                        "exists", intentDefinition.getName());
+                Log.error(errorMessage);
+                throw new DialogFlowException(errorMessage, e);
+            }
+        }
+    }
+
+    /**
+     * Deletes the {@link Intent} matching the provided {@code intentDefinition} from the DialogFlow project.
+     * <p>
+     * <b>Note:</b> this method does not train the underlying DialogFlow Machine Learning Engine, so multiple calls
+     * to this method are not generating multiple training calls. Once all the {@link IntentDefinition}s have been
+     * deleted from the DialogFlow project use {@link #trainMLEngine()} to train the ML Engine.
+     *
+     * @param intentDefinition the {@link IntentDefinition} to delete from the DialogFlow project
+     * @throws DialogFlowException if the {@link DialogFlowApi} is shutdown
+     * @see #trainMLEngine()
+     */
+    public void deleteIntentDefinition(IntentDefinition intentDefinition) {
+        if (isShutdown()) {
+            throw new DialogFlowException(MessageFormat.format("Cannot delete the Intent {0}, the DialogFlow API is " +
+                    "shutdown", intentDefinition.getName()));
+        }
+        checkNotNull(intentDefinition, "Cannot delete the IntentDefinition null");
+        checkNotNull(intentDefinition.getName(), "Cannot delete the IntentDefinition with null as its name");
+        List<Intent> registeredIntents = getRegisteredIntents();
+        for (Intent intent : registeredIntents) {
+            if (intent.getDisplayName().equals(intentDefinition.getName())) {
+                Log.info("Deleting intent {0}", intentDefinition.getName());
+                intentsClient.deleteIntent(intent.getName());
+                Log.info("Intent {0} deleted", intentDefinition.getName());
+                return;
+            }
+        }
+        Log.warn("Cannot delete the Intent {0}, the intent does not exist", intentDefinition.getName());
+    }
+
+    /**
+     * Sends a training query to the DialogFlow ML Engine and waits for its completion.
+     * <p>
+     * This method checks every second whether the underlying ML Engine has finished its training. Note that this
+     * method is blocking as long as the ML Engine training is not terminated, and may not terminate if an issue
+     * occurred on the DialogFlow side.
+     *
+     * @throws DialogFlowException if the {@link DialogFlowApi} is shutdown
+     */
+    public void trainMLEngine() {
+        if (isShutdown()) {
+            throw new DialogFlowException("Cannot train the ML Engine, the DialogFlow API is shutdown");
+        }
+        Log.info("Starting ML Engine Training (this make take a few minutes)");
+        TrainAgentRequest request = TrainAgentRequest.newBuilder()
+                .setParent(projectName.toString())
+                .build();
+        ApiFuture<Operation> future = agentsClient.trainAgentCallable().futureCall(request);
+        try {
+            Operation operation = future.get();
+            while (!operation.getDone()) {
+                Thread.sleep(1000);
+                /*
+                 * Retrieve the new version of the Operation from the API.
+                 */
+                operation = agentsClient.getOperationsClient().getOperation(operation.getName());
+            }
+            Log.info("ML Engine Training completed");
+        } catch (InterruptedException | ExecutionException e) {
+            String errorMessage = "An error occurred during the ML Engine Training";
+            Log.error(errorMessage);
+            throw new DialogFlowException(errorMessage, e);
+        }
+    }
+
+    /**
      * Creates a new DialogFlow session.
      * <p>
      * A DialogFlow session contains contextual information and previous answers for a given client, and should not
@@ -137,16 +350,18 @@ public class DialogFlowApi {
     }
 
     /**
-     * Shuts down the DialogFlow client and invalidates the session.
+     * Shuts down the DialogFlow clients and invalidates the session.
      * <p>
      * <b>Note:</b> calling this method invalidates the DialogFlow connection, and thus this class cannot be used to
      * access DialogFlow API anymore.
      */
     public void shutdown() {
-        if(isShutdown()) {
+        if (isShutdown()) {
             throw new DialogFlowException("Cannot perform shutdown, DialogFlow API is already shutdown");
         }
         this.sessionsClient.shutdownNow();
+        this.intentsClient.shutdownNow();
+        this.agentsClient.shutdownNow();
     }
 
     /**
@@ -155,14 +370,15 @@ public class DialogFlowApi {
      * @return {@code true} if the DialogFlow client is shutdown, {@code false} otherwise
      */
     public boolean isShutdown() {
-        return this.sessionsClient.isShutdown();
+        return this.sessionsClient.isShutdown() || this.intentsClient.isShutdown() || this.agentsClient.isShutdown();
     }
 
     /**
      * Returns the {@link RecognizedIntent} extracted from the provided {@code text}
      * <p>
      * The returned {@link RecognizedIntent} is constructed from the raw {@link Intent} returned by the DialogFlow
-     * API, using the mapping defined in {@link #convertDialogFlowIntent(Intent)}. {@link RecognizedIntent}s are used
+     * API, using the mapping defined in {@link #convertDialogFlowIntentToRecognizedIntent(Intent)}.
+     * {@link RecognizedIntent}s are used
      * to wrap the Intents returned by the Intent Recognition APIs and decouple the application from the concrete API
      * used.
      * <p>
@@ -199,18 +415,40 @@ public class DialogFlowApi {
                 "Detected Intent: {1} (confidence: {2})\n" +
                 "Fulfillment Text: {3}", queryResult.getQueryText(), queryResult.getIntent()
                 .getDisplayName(), queryResult.getIntentDetectionConfidence(), queryResult.getFulfillmentText());
-        return convertDialogFlowIntent(queryResult.getIntent());
+        return convertDialogFlowIntentToRecognizedIntent(queryResult.getIntent());
     }
 
-    private RecognizedIntent convertDialogFlowIntent(Intent intent) {
-        if(nonNull(intent)) {
+    private IntentDefinition convertDialogFlowIntentToIntentDefinition(Intent intent) {
+        if (nonNull(intent)) {
+            return JarvisCore.getInstance().getIntentDefinitionRegistry().getIntentDefinition(intent.getDisplayName());
+            /*
+             * The code below build an IntentDefinition from scratch, but does not check that the intent is
+             * registered. We should move to something like that in the future, in order to let modelers design their
+             * intents in the DialogFlow page if they want. Moving to this kind of architecture does not require an
+             * IntentDefinition factory, or at least not the kind of one we are using now. (we need to synchronize it)
+             */
+//            IntentDefinition intentDefinition = intentFactory.createIntentDefinition();
+//            intentDefinition.setName(intent.getDisplayName());
+//            for(Intent.TrainingPhrase trainingPhrase : intent.getTrainingPhrasesList()) {
+//                for(Intent.TrainingPhrase.Part part : trainingPhrase.getPartsList()) {
+//                    intentDefinition.getTrainingSentences().add(part.getText());
+//                }
+//            }
+//            return intentDefinition;
+        } else {
+            Log.warn("Cannot convert null to IntentDefinition");
+            return null;
+        }
+    }
+
+    private RecognizedIntent convertDialogFlowIntentToRecognizedIntent(Intent intent) {
+        if (nonNull(intent)) {
             RecognizedIntent recognizedIntent = intentFactory.createRecognizedIntent();
             /*
              * Retrieve the IntentDefinition corresponding to this Intent.
              */
-            IntentDefinition intentDefinition = JarvisCore.getInstance().getIntentDefinitionRegistry()
-                    .getIntentDefinition(intent.getDisplayName());
-            if(isNull(intentDefinition)) {
+            IntentDefinition intentDefinition = convertDialogFlowIntentToIntentDefinition(intent);
+            if (isNull(intentDefinition)) {
                 String errorMessage = MessageFormat.format("Cannot retrieve the IntentDefinition associated to the " +
                         "provided DialogFlow Intent {0}", intent.getDisplayName());
                 Log.error(errorMessage);
@@ -220,15 +458,15 @@ public class DialogFlowApi {
             /*
              * Set the output context values.
              */
-            if(intent.getOutputContextsCount() > 0) {
-                if(intent.getOutputContextsCount() > 1) {
+            if (intent.getOutputContextsCount() > 0) {
+                if (intent.getOutputContextsCount() > 1) {
                     Log.warn("Multiple output contexts are not supported for now, proceeding with the first context " +
                             "found");
                 }
                 Context outContext = intent.getOutputContexts(0);
                 Collection<Object> outContextValues = outContext.getParameters().getAllFields().values();
-                for(Object value : outContextValues) {
-                    if(value instanceof String) {
+                for (Object value : outContextValues) {
+                    if (value instanceof String) {
                         recognizedIntent.getOutContextValues().add((String) value);
                     } else {
                         throw new UnsupportedOperationException("Only String output context values are supported for " +
@@ -247,14 +485,20 @@ public class DialogFlowApi {
 
     /**
      * Closes the DialogFlow session if it is not shutdown yet.
-     *
-     * @throws Throwable
      */
     @Override
-    protected void finalize() throws Throwable {
+    protected void finalize() {
         if (!sessionsClient.isShutdown()) {
             Log.warn("DialogFlow session was not closed properly, calling automatic shutdown");
             this.sessionsClient.shutdownNow();
+        }
+        if (!intentsClient.isShutdown()) {
+            Log.warn("DialogFlow Intent client was not closed properly, calling automatic shutdown");
+            this.intentsClient.shutdownNow();
+        }
+        if (!agentsClient.isShutdown()) {
+            Log.warn("DialogFlow Agent client was not closed properly, calling automatic shutdown");
+            this.agentsClient.shutdownNow();
         }
     }
 }
--- core/src/test/java/fr/zelus/jarvis/dialogflow/DialogFlowApiTest.java
+++ core/src/test/java/fr/zelus/jarvis/dialogflow/DialogFlowApiTest.java
@@ -1,5 +1,6 @@
 package fr.zelus.jarvis.dialogflow;
 
+import com.google.cloud.dialogflow.v2.Intent;
 import com.google.cloud.dialogflow.v2.SessionName;
 import fr.zelus.jarvis.core.JarvisCore;
 import fr.zelus.jarvis.intent.IntentDefinition;
@@ -14,10 +15,15 @@ import fr.zelus.jarvis.orchestration.OrchestrationLink;
 import fr.zelus.jarvis.orchestration.OrchestrationModel;
 import fr.zelus.jarvis.stubs.StubInputProvider;
 import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.Objects.nonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -32,6 +38,18 @@ public class DialogFlowApiTest {
     protected static Class<StubInputProvider> VALID_INPUT_PROVIDER_CLAZZ = StubInputProvider.class;
 
     protected DialogFlowApi api;
+
+    /**
+     * Stores the last {@link IntentDefinition} registered by
+     * {@link DialogFlowApi#registerIntentDefinition(IntentDefinition)}.
+     * <p>
+     * <b>Note:</b> this variable must be set by each test case calling
+     * {@link DialogFlowApi#registerIntentDefinition(IntentDefinition)}, to enable their deletion in the
+     * {@link #tearDown()} method. Not setting this variable would add test-related intents in the DialogFlow project.
+     *
+     * @see #tearDown()
+     */
+    private IntentDefinition registeredIntentDefinition;
 
     // not tested here, only instantiated to enable IntentDefinition registration and Module retrieval
     protected static JarvisCore jarvisCore;
@@ -56,7 +74,19 @@ public class DialogFlowApiTest {
         actionInstance.setAction(stubAction);
         link.getActions().add(actionInstance);
         orchestrationModel.getOrchestrationLinks().add(link);
-        jarvisCore = new JarvisCore(VALID_PROJECT_ID, VALID_LANGUAGE_CODE, orchestrationModel, VALID_INPUT_PROVIDER_CLAZZ);
+        jarvisCore = new JarvisCore(VALID_PROJECT_ID, VALID_LANGUAGE_CODE, orchestrationModel,
+                VALID_INPUT_PROVIDER_CLAZZ);
+    }
+
+    @After
+    public void tearDown() {
+        if (nonNull(registeredIntentDefinition)) {
+            api.deleteIntentDefinition(registeredIntentDefinition);
+        }
+        /*
+         * Reset the variable value to null to avoid unnecessary deletion calls.
+         */
+        registeredIntentDefinition = null;
     }
 
     @Rule
@@ -90,6 +120,108 @@ public class DialogFlowApiTest {
         api = new DialogFlowApi(VALID_PROJECT_ID);
         softly.assertThat(VALID_PROJECT_ID).as("Valid project ID").isEqualTo(api.getProjectId());
         softly.assertThat(VALID_LANGUAGE_CODE).as("Valid language code").isEqualTo(api.getLanguageCode());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerIntentDefinitionNullIntentDefinition() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        api.registerIntentDefinition(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void registerIntentDefinitionNullName() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        IntentDefinition intentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        api.registerIntentDefinition(intentDefinition);
+    }
+
+    @Test
+    public void registerIntentDefinitionValidIntentDefinition() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        registeredIntentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        String intentName = UUID.randomUUID().toString();
+        String trainingPhrase = "test";
+        registeredIntentDefinition.setName(intentName);
+        registeredIntentDefinition.getTrainingSentences().add(trainingPhrase);
+        api.registerIntentDefinition(registeredIntentDefinition);
+        List<Intent> registeredIntents = api.getRegisteredIntentsFullView();
+        assertThat(registeredIntents).as("Registered Intent list is not null").isNotNull();
+        softly.assertThat(registeredIntents).as("Registered Intent list is not empty").isNotEmpty();
+        Intent foundIntent = null;
+        for (Intent intent : registeredIntents) {
+            if (intent.getDisplayName().equals(intentName)) {
+                foundIntent = intent;
+            }
+        }
+        assertThat(foundIntent).as("Registered Intent list contains the registered IntentDefinition")
+                .isNotNull();
+        softly.assertThat(foundIntent.getTrainingPhrasesList()).as("Intent's training phrase list is not empty")
+                .isNotEmpty();
+        boolean foundTrainingPhrase = false;
+        for (Intent.TrainingPhrase intentTrainingPhrase : foundIntent.getTrainingPhrasesList()) {
+            for (Intent.TrainingPhrase.Part part : intentTrainingPhrase.getPartsList()) {
+                if (part.getText().equals(trainingPhrase)) {
+                    foundTrainingPhrase = true;
+                }
+            }
+        }
+        softly.assertThat(foundTrainingPhrase).as("The IntentDefinition's training phrase is in the retrieved " +
+                "Intent").isTrue();
+    }
+
+    @Test
+    public void registerIntentDefinitionAlreadyRegistered() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        registeredIntentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        String intentName = UUID.randomUUID().toString();
+        registeredIntentDefinition.setName(intentName);
+        registeredIntentDefinition.getTrainingSentences().add("test");
+        registeredIntentDefinition.getTrainingSentences().add("test jarvis");
+        api.registerIntentDefinition(registeredIntentDefinition);
+        api.registerIntentDefinition(registeredIntentDefinition);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void deleteIntentDefinitionNullIntentDefinition() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        api.deleteIntentDefinition(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void deleteIntentDefinitionNullName() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        IntentDefinition intentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        api.deleteIntentDefinition(intentDefinition);
+    }
+
+    @Test
+    public void deleteIntentDefinitionNotRegisteredIntent() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        String intentName = UUID.randomUUID().toString();
+        IntentDefinition intentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        intentDefinition.setName(intentName);
+        int registeredIntentsCount = api.getRegisteredIntents().size();
+        api.deleteIntentDefinition(intentDefinition);
+        assertThat(api.getRegisteredIntents()).as("Registered Intents list has not changed").hasSize
+                (registeredIntentsCount);
+    }
+
+    @Test
+    public void deleteIntentDefinitionRegisteredIntentDefinition() {
+        api = new DialogFlowApi(VALID_PROJECT_ID);
+        String intentName = UUID.randomUUID().toString();
+        registeredIntentDefinition = IntentFactory.eINSTANCE.createIntentDefinition();
+        registeredIntentDefinition.setName(intentName);
+        api.registerIntentDefinition(registeredIntentDefinition);
+        api.deleteIntentDefinition(registeredIntentDefinition);
+        Intent foundIntent = null;
+        for(Intent intent : api.getRegisteredIntents()) {
+            if(intent.getDisplayName().equals(intentName)) {
+                foundIntent = intent;
+            }
+        }
+        assertThat(foundIntent).as("The Intent has been removed from the registered Intents").isNull();
+
     }
 
     @Test
