a24e522a80012ac0d677ab2804985d2dd69c7444
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-07-09 17:23:05 +0200

    add MailModule javadoc (fix #57)

--- core/src/main/java/fr/zelus/jarvis/plugins/mail/module/MailModule.java
+++ core/src/main/java/fr/zelus/jarvis/plugins/mail/module/MailModule.java
@@ -23,16 +23,56 @@ import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A {@link JarvisModule} concrete implementation providing mailing functionality that can be used in orchestration
+ * models.
+ * <p>
+ * This module defines a single action {@link fr.zelus.jarvis.plugins.mail.module.action.SendMail} that sends a mail
+ * through the {@link Gmail} service to a given address.
+ * <p>
+ * This class is part of jarvis' core modules, and can be used in an orchestration model by importing the <i>core
+ * .MailModule</i> package.
+ *
+ * @see fr.zelus.jarvis.plugins.mail.module.action.SendMail
+ */
 public class MailModule extends JarvisModule {
 
+    /**
+     * The name of the application used to initialize the {@link Gmail} service.
+     */
     private static final String APPLICATION_NAME = "Jarvis";
+
+    /**
+     * The {@link JsonFactory} instance used to parse messages from the {@link Gmail} service.
+     */
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /**
+     * The folder used to store the {@link Gmail} application credentials.
+     */
     private static final String CREDENTIALS_FOLDER = "credentials"; // Directory to store user credentials.
+
+    /**
+     * The OAuth scopes required by the application.
+     */
     private static final List<String> SCOPES = Collections.singletonList(GmailScopes.MAIL_GOOGLE_COM);
+
+    /**
+     * The path of the secret API key used to access the {@link Gmail} service.
+     * <p>
+     * This information should be provided by the module's {@link org.apache.commons.configuration2.Configuration},
+     * or retrieved from the system environment variables (see #62).
+     */
     private static final String CLIENT_SECRET_DIR = "/gmail_secret.json";
 
+    /**
+     * The {@link Gmail} service used to send mails.
+     */
     private Gmail service;
 
+    /**
+     * Constructs a new {@link MailModule} and initializes the underlying {@link Gmail} service.
+     */
     public MailModule() {
         super();
         try {
@@ -40,16 +80,19 @@ public class MailModule extends JarvisModule {
             service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
-        } catch(IOException | GeneralSecurityException e) {
+        } catch (IOException | GeneralSecurityException e) {
             String errorMessage = MessageFormat.format("Cannot start {0}", this.getClass().getSimpleName());
             throw new JarvisException(errorMessage, e);
         }
     }
 
-    public Gmail getService() {
-        return service;
-    }
-
+    /**
+     * Loads the application secrets and build the associated credentials.
+     *
+     * @param HTTP_TRANSPORT the {@link NetHttpTransport} used to build the application authorization
+     * @return the built {@link Credential}
+     * @throws IOException if an error occurred when accessing the remote {@link Gmail} service
+     */
     private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = this.getClass().getResourceAsStream(CLIENT_SECRET_DIR);
@@ -64,6 +107,14 @@ public class MailModule extends JarvisModule {
         return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     }
 
+    /**
+     * Returns the {@link Gmail} service associated to this module.
+     *
+     * @return the {@link Gmail} service associated to this module
+     */
+    public Gmail getService() {
+        return service;
+    }
 
 
 }
--- core/src/main/java/fr/zelus/jarvis/plugins/mail/module/action/SendMail.java
+++ core/src/main/java/fr/zelus/jarvis/plugins/mail/module/action/SendMail.java
@@ -19,12 +19,43 @@ import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.Properties;
 
+/**
+ * A {@link JarvisAction} that sends an email to the provided {@code address} with the given {@code body} and {@code
+ * title}.
+ * <p>
+ * This class relies on the {@link MailModule}'s {@link Gmail} service to connect to the corresponding Gmail account
+ * and send emails.
+ *
+ * @see MailModule
+ */
 public class SendMail extends JarvisAction<MailModule> {
 
+    /**
+     * The address to send the email to.
+     */
     private String to;
+
+    /**
+     * The title of the email to send.
+     */
     private String title;
+
+    /**
+     * The content of the email to send.
+     */
     private String body;
 
+    /**
+     * Constructs a new {@link SendMail} with the provided {@code containingModule}, {@code context}, {@code to},
+     * {@code title}, and {@code body}.
+     *
+     * @param containingModule the {@link MailModule} containing this action
+     * @param context          the {@link JarvisContext} associated to this action
+     * @param to               the address to send the email to
+     * @param title            the title of the email to send
+     * @param body             the content of the email to send
+     * @throws NullPointerException if the provided {@code containingModule} or {@code context} is {@code null}
+     */
     public SendMail(MailModule containingModule, JarvisContext context, String to, String title, String body) {
         super(containingModule, context);
         this.to = to;
@@ -32,6 +63,14 @@ public class SendMail extends JarvisAction<MailModule> {
         this.body = body;
     }
 
+    /**
+     * Sends an email to the provided {@code to} address with the given {@code title} and {@code body}.
+     * <p>
+     * This method relies on the containing {@link MailModule}'s {@link Gmail} service to authenticate the bot and
+     * send emails.
+     *
+     * @return {@code null}
+     */
     @Override
     public Object call() {
         try {
@@ -46,13 +85,16 @@ public class SendMail extends JarvisAction<MailModule> {
         return null;
     }
 
-    private Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws MessagingException,
-            IOException {
-        Message message = createMessageWithEmail(emailContent);
-        message = service.users().messages().send(userId, message).execute();
-        return message;
-    }
-
+    /**
+     * Creates a {@link MimeMessage} from the given {@code to}, {@code from}, {@code subject}, and {@code bodyText}.
+     *
+     * @param to       the address to send the email to
+     * @param from     the bot address
+     * @param subject  the title of the email to send
+     * @param bodyText the content of the email to send
+     * @return the created {@link MimeMessage}
+     * @throws MessagingException if an error occurred when building the {@link MimeMessage}
+     */
     public static MimeMessage createEmail(String to, String from, String subject, String bodyText) throws
             MessagingException {
         Properties props = new Properties();
@@ -65,6 +107,14 @@ public class SendMail extends JarvisAction<MailModule> {
         return email;
     }
 
+    /**
+     * Creates a {@link Gmail} compatible {@link Message} from the provided {@code emailContent}.
+     *
+     * @param emailContent the {@link MimeMessage} to construct a {@link Gmail} {@link Message} from
+     * @return the created {@link Message}
+     * @throws MessagingException if an error occurred when building the {@link Message}
+     * @throws IOException        if an error occurred when building the {@link Message}
+     */
     public static Message createMessageWithEmail(MimeMessage emailContent) throws MessagingException, IOException {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         emailContent.writeTo(buffer);
@@ -74,4 +124,22 @@ public class SendMail extends JarvisAction<MailModule> {
         message.setRaw(encodedEmail);
         return message;
     }
+
+    /**
+     * Sends the provided {@code emailContent} using the given {@link Gmail} service.
+     *
+     * @param service      the {@link Gmail} service used to send the email
+     * @param userId       the identifier used to connect to the {@link Gmail} service
+     * @param emailContent the {@link MimeMessage} to send
+     * @return the send {@link Message}
+     * @throws MessagingException if an error occurred when sending the email
+     * @throws IOException        if an error occurred when sending the email
+     */
+    private Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws MessagingException,
+            IOException {
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+        return message;
+    }
+
 }
