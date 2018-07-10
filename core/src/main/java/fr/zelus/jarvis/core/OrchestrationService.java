f4ff7229a6a1f0db1dc5dc09c22d49e03dd4da57
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-07-10 12:58:45 +0200

    update OrchestrationService#getActionsFromIntent (fix #66)

--- core/src/main/java/fr/zelus/jarvis/core/OrchestrationService.java
+++ core/src/main/java/fr/zelus/jarvis/core/OrchestrationService.java
@@ -3,7 +3,6 @@ package fr.zelus.jarvis.core;
 import fr.zelus.jarvis.core.session.JarvisSession;
 import fr.zelus.jarvis.intent.IntentDefinition;
 import fr.zelus.jarvis.intent.RecognizedIntent;
-import fr.zelus.jarvis.module.Action;
 import fr.zelus.jarvis.orchestration.ActionInstance;
 import fr.zelus.jarvis.orchestration.OrchestrationLink;
 import fr.zelus.jarvis.orchestration.OrchestrationModel;
@@ -12,17 +11,15 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Reads an {@link OrchestrationModel} and returns the {@link JarvisAction}s associated to the provided
+ * Reads an {@link OrchestrationModel} and returns the {@link ActionInstance}s associated to the provided
  * {@link RecognizedIntent}s.
  * <p>
- * This class is used by {@link JarvisCore} to retrieve the {@link JarvisAction} to execute when a
- * {@link RecognizedIntent} has been computed from an user input. The {@link OrchestrationService} takes care of the
- * {@link JarvisAction} construction, and returns an valid action instances to be executed by the {@link JarvisCore}
- * component.
+ * This class is used by {@link JarvisCore} to create the {@link JarvisAction} to execute when a
+ * {@link RecognizedIntent} has been computed from an user input.
  *
- * @see JarvisCore
- * @see JarvisAction
+ * @see ActionInstance
  * @see RecognizedIntent
+ * @see JarvisCore
  */
 public class OrchestrationService {
 
@@ -51,40 +48,22 @@ public class OrchestrationService {
     }
 
     /**
-     * Retrieves and instantiate the {@link JarvisAction}s associated to the provided {@code recognizedIntent}.
+     * Retrieves the {@link ActionInstance}s associated to the provided {@code recognizedIntent}.
      * <p>
-     * This class navigates the underlying {@link OrchestrationModel} and retrieves the {@link Action} associated to
-     * the provided {@code recognizedIntent}. These {@link Action}s are then reified into {@link JarvisAction}s and
-     * instantiated by using the {@code recognizedIntent}'s values.
+     * This class navigates the underlying {@link OrchestrationModel} and retrieves the {@link ActionInstance}s
+     * associated to the provided {@code recognizedIntent}. These {@link ActionInstance}s are used by the core
+     * component to create the concrete {@link JarvisAction} to execute.
      *
-     * @param recognizedIntent the {@link RecognizedIntent} to retrieve the {@link JarvisAction}s from
-     * @return a {@link List} containing the instantiated {@link JarvisAction}s associated to the provided {@code
+     * @param recognizedIntent the {@link RecognizedIntent} to retrieve the {@link ActionInstance}s from
+     * @return a {@link List} containing the instantiated {@link ActionInstance}s associated to the provided {@code
      * recognizedIntent}.
-     * @see JarvisModuleRegistry#getJarvisModule(String)
-     * @see JarvisModule#createJarvisAction(ActionInstance, RecognizedIntent, fr.zelus.jarvis.core.session
-     * .JarvisSession)
+     * @see JarvisModule#createJarvisAction(ActionInstance, RecognizedIntent, JarvisSession)
      */
-    public List<ActionInstance> getActionsFromIntent(RecognizedIntent recognizedIntent, JarvisSession session) {
+    public List<ActionInstance> getActionsFromIntent(RecognizedIntent recognizedIntent) {
         IntentDefinition intentDefinition = recognizedIntent.getDefinition();
-//        List<Class<? extends JarvisAction>> jarvisActions = new ArrayList<>();
         for (OrchestrationLink link : orchestrationModel.getOrchestrationLinks()) {
             if (link.getIntent().getName().equals(intentDefinition.getName())) {
                 return link.getActions();
-                // The link refers to this Intent
-//                for (ActionInstance actionInstance : link.getActions()) {
-//                    Action baseAction = actionInstance.getAction();
-//                    JarvisModule jarvisModule = JarvisCore.getInstance().getJarvisModuleRegistry().getJarvisModule(
-//                            (Module) baseAction.eContainer());
-////                    JarvisAction jarvisAction = jarvisModule.createJarvisAction(actionInstance, recognizedIntent,
-////                            context);
-//                    Class<? extends JarvisAction> jarvisActionClass = jarvisModule.getAction(actionInstance.getAction
-//                            ().getName());
-//                    if (isNull(jarvisActionClass)) {
-//                        Log.warn("Cannot retrieve the JarvisAction", actionInstance,
-//                                recognizedIntent);
-//                    }
-//                    jarvisActions.add(jarvisActionClass);
-//                }
             }
         }
         return Collections.emptyList();
