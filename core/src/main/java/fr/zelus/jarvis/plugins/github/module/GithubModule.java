6750e35f72ddaa052976ac51e95e99de186909b8
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-09-26 14:23:44 +0200

    add Configuration parameter in GithubModule and add github.module
    This configuration is required to initialize the GithubWebhookEventProvider (refs #144)

--- core/src/main/java/fr/zelus/jarvis/plugins/github/module/GithubModule.java
+++ core/src/main/java/fr/zelus/jarvis/plugins/github/module/GithubModule.java
@@ -2,6 +2,7 @@ package fr.zelus.jarvis.plugins.github.module;
 
 import fr.zelus.jarvis.core.JarvisCore;
 import fr.zelus.jarvis.core.JarvisModule;
+import org.apache.commons.configuration2.Configuration;
 
 public class GithubModule extends JarvisModule {
 
@@ -9,9 +10,11 @@ public class GithubModule extends JarvisModule {
      * Constructs a new {@link GithubModule} from the provided {@link JarvisCore}.
      *
      * @param jarvisCore the {@link JarvisCore} instance associated to this module
-     * @throws NullPointerException if the provided {@code jarvisCore} is {@code null}
+     * @param configuration the {@link Configuration} used to customize Github events and actions
+
+     * @throws NullPointerException if the provided {@code jarvisCore} or {@code configuration} is {@code null}
      */
-    public GithubModule(JarvisCore jarvisCore) {
-        super(jarvisCore);
+    public GithubModule(JarvisCore jarvisCore, Configuration configuration) {
+        super(jarvisCore, configuration);
     }
 }
