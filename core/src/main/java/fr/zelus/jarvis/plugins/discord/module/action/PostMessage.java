ee4ce9d4496066f011a250318e10db1468447095
Gwendal Daniel <gwendal.daniel@gmail.com>
2018-09-27 15:09:55 +0200

    add fallback discord private channel creation
    Opening a private channel is required when the bot initiate the conversation. In this case the provided channel ID must be a valid user ID. This allows generic post message actions to initiate conversations that have started on another platform.

--- core/src/main/java/fr/zelus/jarvis/plugins/discord/module/action/PostMessage.java
+++ core/src/main/java/fr/zelus/jarvis/plugins/discord/module/action/PostMessage.java
@@ -1,12 +1,18 @@
 package fr.zelus.jarvis.plugins.discord.module.action;
 
+import fr.inria.atlanmod.commons.log.Log;
 import fr.zelus.jarvis.core.JarvisAction;
+import fr.zelus.jarvis.core.JarvisException;
 import fr.zelus.jarvis.core.JarvisMessageAction;
 import fr.zelus.jarvis.core.session.JarvisSession;
 import fr.zelus.jarvis.plugins.discord.module.DiscordModule;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
+import java.text.MessageFormat;
 
 import static fr.inria.atlanmod.commons.Preconditions.checkArgument;
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 /**
@@ -41,6 +47,21 @@ public class PostMessage extends JarvisMessageAction<DiscordModule> {
         checkArgument(nonNull(channel) && !channel.isEmpty(), "Cannot construct a {0} action with the provided " +
                 "channel {1}, expected a non-null and not empty String", this.getClass().getSimpleName(), channel);
         this.channel = module.getJdaClient().getPrivateChannelById(channel);
+        if (isNull(this.channel)) {
+            /*
+             * The channel may be null if the provided ID corresponds to a user ID, in that case we can open a new
+             * private channel by first retrieving the Discord User associated to the provided ID.
+             */
+            User user = module.getJdaClient().getUserById(channel);
+            if (nonNull(user)) {
+                Log.info("Opening a private channel with user {0} (id: {1})", user.getName(), user.getId());
+                this.channel = user.openPrivateChannel().complete();
+            } else {
+                throw new JarvisException(MessageFormat.format("Cannot construct the %s action: the provided channel " +
+                        "ID ({0}) does not correspond to an existing private channel or a valid Discord user",
+                        channel));
+            }
+        }
     }
 
     /**
