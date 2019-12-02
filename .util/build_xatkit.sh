96219f5f1bb130ee530f9df7d44fb79dbef98dae
Gwendal Daniel <gwendal.daniel@gmail.com>
2019-12-02 02:12:11 +0100

    update cd command in travis build script

--- .util/build_xatkit.sh
+++ .util/build_xatkit.sh
@@ -11,7 +11,7 @@ main() {
     e "Building Xatkit Parent"
     cd /tmp
     git clone https://github.com/xatkit-bot-platform/xatkit.git > /dev/null
-    cd xatkit-releases
+    cd xatkit
     mvn install -DskipTests > /dev/null
     e "Done"
 
