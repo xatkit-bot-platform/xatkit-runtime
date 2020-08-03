74907a49d046f5ff443bed4f17e3346f15eb8e7e
Gwendal Daniel <gwendal.daniel@gmail.com>
2020-08-03 16:15:48 +0200

    fix TransitionDelegate to avoid transition overriding

--- core/src/main/java/com/xatkit/dsl/state/impl/TransitionDelegate.java
+++ core/src/main/java/com/xatkit/dsl/state/impl/TransitionDelegate.java
@@ -8,6 +8,7 @@ import com.xatkit.dsl.state.OptionalWhenStep;
 import com.xatkit.dsl.state.StateProvider;
 import com.xatkit.dsl.state.TransitionStep;
 import com.xatkit.dsl.state.WhenStep;
+import com.xatkit.execution.ExecutionFactory;
 import com.xatkit.execution.State;
 import com.xatkit.execution.StateContext;
 import com.xatkit.execution.Transition;
@@ -18,6 +19,8 @@ import com.xatkit.util.IsIntentDefinitionPredicate;
 import lombok.NonNull;
 
 import java.util.function.Predicate;
+
+import static java.util.Objects.nonNull;
 
 public class TransitionDelegate extends StateDelegate implements
         TransitionStep,
@@ -50,12 +53,38 @@ public class TransitionDelegate extends StateDelegate implements
 
     @Override
     public @NonNull MoveToStep when(@NonNull Predicate<StateContext> condition) {
+        if(nonNull(this.transition.getState())) {
+            /*
+             * The current transition already contains a "moveTo" state. This invocation of when() means that we are
+             * creating another transition:
+             * next()
+             *   .when(...).moveTo(...)
+             *   .when(...) // Here we are defining a new transition.
+             * We need to create a new Transition instance and return a new delegate to reflect this behavior.
+             */
+            Transition newTransition = ExecutionFactory.eINSTANCE.createTransition();
+            this.state.getTransitions().add(newTransition);
+            return new TransitionDelegate(this.state, newTransition);
+        }
         this.transition.setCondition(condition);
         return this;
     }
 
     @Override
     public EventPredicateStep when() {
+        if(nonNull(this.transition.getState())) {
+            /*
+             * The current transition already contains a "moveTo" state. This invocation of when() means that we are
+             * creating another transition:
+             * next()
+             *   .when(...).moveTo(...)
+             *   .when(...) // Here we are defining a new transition.
+             * We need to create a new Transition instance and return a new delegate to reflect this behavior.
+             */
+            Transition newTransition = ExecutionFactory.eINSTANCE.createTransition();
+            this.state.getTransitions().add(newTransition);
+            return new TransitionDelegate(this.state, newTransition);
+        }
         return this;
     }
 
