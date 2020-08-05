97f3c4829ba5677e13da1d2daa1cb20001844bac
Gwendal Daniel <gwendal.daniel@gmail.com>
2020-08-05 16:12:48 +0200

    remove Xatkit class
    This class is not needed anymore: we don't want to start external bots, bots are now starting themselves.

--- core/src/main/javadoc/com/xatkit/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Xatkit base package.
- * <p>
- * This package define the top-level {@link com.xatkit.Xatkit} class, that allows to start a Xatkit bot with a
- * given execution model.
- */
-package com.xatkit;
