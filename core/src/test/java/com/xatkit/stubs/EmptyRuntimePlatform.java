5fcd561df5a1b9c6bda5d44a3c3c19a5ed356b42
Gwendal Daniel <gwendal.daniel@gmail.com>
2020-08-05 18:41:11 +0200

    update stubbed platforms
    They should be removed later, but for now they are needed in some test cases.

--- core/src/test/java/com/xatkit/stubs/EmptyRuntimePlatform.java
+++ core/src/test/java/com/xatkit/stubs/EmptyRuntimePlatform.java
@@ -1,28 +1,13 @@
 package com.xatkit.stubs;
 
-import com.xatkit.core.XatkitCore;
 import com.xatkit.core.platform.RuntimePlatform;
-import com.xatkit.core.session.XatkitSession;
-import org.eclipse.xtext.xbase.XMemberFeatureCall;
-
-import java.util.List;
 
 /**
  * An empty {@link RuntimePlatform} used to test {@link RuntimePlatform}-related methods.
- * <p>
- * See {@link StubRuntimePlatform} to create a stub {@link RuntimePlatform} that provided preset
- * {@link StubRuntimePlatform#getAction()} and
- * {@link StubRuntimePlatform#createRuntimeAction(XMemberFeatureCall, List, XatkitSession)} methods.
  */
 public class EmptyRuntimePlatform extends RuntimePlatform {
 
-    /**
-     * Constructs a new {@link EmptyRuntimePlatform} from the provided {@link XatkitCore}.
-     *
-     * @param xatkitCore the {@link XatkitCore} instance associated to this runtimePlatform
-     * @throws NullPointerException if the provided {@code xatkitCore} is {@code null}
-     */
-    public EmptyRuntimePlatform(XatkitCore xatkitCore) {
-        super(xatkitCore);
+    public EmptyRuntimePlatform() {
+        super();
     }
 }
--- core/src/test/java/com/xatkit/stubs/StubRuntimePlatform.java
+++ core/src/test/java/com/xatkit/stubs/StubRuntimePlatform.java
@@ -1,17 +1,8 @@
 package com.xatkit.stubs;
 
-import com.xatkit.core.XatkitCore;
 import com.xatkit.core.platform.RuntimePlatform;
-import com.xatkit.core.platform.action.RuntimeAction;
-import com.xatkit.core.session.XatkitSession;
 import com.xatkit.stubs.action.ErroringStubRuntimeAction;
 import com.xatkit.stubs.action.StubRuntimeAction;
-import com.xatkit.util.ExecutionModelUtils;
-import org.apache.commons.configuration2.Configuration;
-import org.eclipse.xtext.xbase.XMemberFeatureCall;
-
-import java.text.MessageFormat;
-import java.util.List;
 
 public class StubRuntimePlatform extends RuntimePlatform {
 
@@ -19,8 +10,7 @@ public class StubRuntimePlatform extends RuntimePlatform {
 
     private ErroringStubRuntimeAction erroringRuntimeAction;
 
-    public StubRuntimePlatform(XatkitCore xatkitCore, Configuration configuration) {
-        super(xatkitCore, configuration);
+    public StubRuntimePlatform() {
         init();
     }
 
@@ -35,19 +25,6 @@ public class StubRuntimePlatform extends RuntimePlatform {
 
     public ErroringStubRuntimeAction getErroringAction() {
         return erroringRuntimeAction;
-    }
-
-    @Override
-    public RuntimeAction createRuntimeAction(XMemberFeatureCall actionCall, List<Object> arguments,
-                                             XatkitSession session) {
-        if (ExecutionModelUtils.getActionName(actionCall).equals("StubRuntimeAction")) {
-            return runtimeAction;
-        } else if (ExecutionModelUtils.getActionName(actionCall).equals("ErroringStubRuntimeAction")) {
-            return erroringRuntimeAction;
-        } else {
-            throw new RuntimeException(MessageFormat.format("Cannot create the action {0}",
-                    ExecutionModelUtils.getActionName(actionCall)));
-        }
     }
 
 }
