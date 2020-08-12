4892cfd10dd924a87863f41710ccde5a713012c3
Hamza <hamza.eddouibi@gmail.com>
2020-08-13 00:14:57 +0200

    Create a builder for Intent class

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/model/Intent.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/model/Intent.java
@@ -2,6 +2,7 @@ package com.xatkit.core.recognition.nlpjs.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -11,11 +12,56 @@ public class Intent {
     private List<IntentExample> examples;
     private List<IntentParameter> parameters;
 
-    public Intent(){}
 
-    public Intent(String intentName, List<IntentExample> examples, List<IntentParameter> parameters) {
-        this.intentName = intentName;
-        this.examples = examples;
-        this.parameters = parameters;
+    public static Builder newBuilder(){
+        return new Intent.Builder();
     }
+
+
+    public static class Builder {
+        private String _intentName;
+        private List<IntentExample> _examples;
+        private List<IntentParameter> _parameters;
+
+        public Builder(){
+            _examples = new ArrayList<>();
+            _parameters = new ArrayList<>();
+        }
+
+        public Intent.Builder intentName(String intentName){
+            _intentName = intentName;
+            return this;
+        }
+
+        public Intent.Builder examples(List<IntentExample> examples){
+            _examples = examples;
+            return this;
+        }
+
+        public Intent.Builder addExample(IntentExample intentExample){
+            _examples.add(intentExample);
+            return this;
+        }
+
+        public Intent.Builder parameters(List<IntentParameter> parameters){
+            _parameters = parameters;
+            return this;
+        }
+
+        public Intent.Builder addExample(IntentParameter intentParameter){
+            _parameters.add(intentParameter);
+            return this;
+        }
+
+        public Intent build(){
+            Intent intent = new Intent();
+            intent.setIntentName(_intentName);
+            intent.setExamples(_examples);
+            intent.setParameters(_parameters);
+            return intent;
+
+        }
+
+    }
+
 }
