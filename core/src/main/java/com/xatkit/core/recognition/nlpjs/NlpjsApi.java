429ef0a1ccb1c76e13360b7d800eb654f1d3dfdb
Hamza <hamza.eddouibi@gmail.com>
2020-08-13 23:07:45 +0200

    Add @NonNull annotation + modify getIntent

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/NlpjsApi.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/NlpjsApi.java
@@ -1,6 +1,7 @@
 package com.xatkit.core.recognition.nlpjs;
 
 import com.xatkit.core.recognition.nlpjs.model.*;
+import lombok.NonNull;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -10,16 +11,16 @@ import retrofit2.http.Path;
 public interface NlpjsApi {
 
     @GET("agent/{agentId}")
-    Call<Agent> getAgentInfo(@Path("agentId") String agentId);
+    Call<Agent> getAgentInfo(@Path("agentId") @NonNull String agentId);
 
     @POST("agent")
-    Call<Void> createAgent(@Body AgentInit agentInit);
+    Call<Void> createAgent(@Body @NonNull AgentInit agentInit);
 
     @POST("agent/{agentId}/train")
-    Call<Void> trainAgent(@Path("agentId") String agentId, @Body TrainingData trainingData);
+    Call<Void> trainAgent(@Path("agentId") @NonNull String agentId, @Body TrainingData trainingData);
 
     @POST("agent/{agentId}/process")
-    Call<RecognizedIntent> getIntent(@Path("agentId") String agentId, @Body UserMessage userMessage);
+    Call<RecognitionResult> getIntent(@Path("agentId") @NonNull String agentId, @Body @NonNull UserMessage userMessage);
 
 
 }
