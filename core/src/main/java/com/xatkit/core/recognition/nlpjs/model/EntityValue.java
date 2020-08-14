d941d85ea1e4a663dc3b85d55af708d4c5f863ab
Hamza <hamza.eddouibi@gmail.com>
2020-08-14 11:21:53 +0200

    Add a builder to EntityValue

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/model/EntityValue.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/model/EntityValue.java
@@ -2,6 +2,7 @@ package com.xatkit.core.recognition.nlpjs.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -9,11 +10,38 @@ public class EntityValue {
     private String value;
     private List<String> synonyms;
 
-    public EntityValue(){
+    public static Builder newBuilder() {
+        return new Builder();
     }
 
-    public EntityValue(String value, List<String> synonyms) {
-        this.value = value;
-        this.synonyms = synonyms;
+    public static class Builder {
+        private String _value;
+        private List<String> _synonyms;
+
+        public Builder() {
+            this._synonyms = new ArrayList<>();
+        }
+
+        public Builder value(String value) {
+            this._value = value;
+            return this;
+        }
+
+        public Builder synonyms(List<String> _synonyms) {
+            this._synonyms = _synonyms;
+            return this;
+        }
+
+        public Builder addSynonym(String synonym) {
+            this._synonyms.add(synonym);
+            return this;
+        }
+
+        public EntityValue build() {
+            EntityValue entityValue = new EntityValue();
+            entityValue.setValue(_value);
+            entityValue.setSynonyms(_synonyms);
+            return entityValue;
+        }
     }
 }
