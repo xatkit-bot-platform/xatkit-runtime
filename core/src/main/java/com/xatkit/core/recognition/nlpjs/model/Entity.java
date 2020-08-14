7a73ac16682ba509d9f3cd26db48bdbd1e53bb47
Hamza <hamza.eddouibi@gmail.com>
2020-08-14 11:20:36 +0200

    Add a builder to Entity

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/model/Entity.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/model/Entity.java
@@ -2,20 +2,57 @@ package com.xatkit.core.recognition.nlpjs.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Entity {
     private String entityName;
-    private String type;
+    private EntityType type;
     private List<EntityValue> references;
 
-    public Entity(){
+    public static Builder newBuilder() {
+        return new Builder();
     }
 
-    public Entity(String entityName, String type, List<EntityValue> references) {
-        this.entityName = entityName;
-        this.type = type;
-        this.references = references;
+
+    public static class Builder {
+        private String _entityName;
+        private EntityType _type;
+        private List<EntityValue> _references;
+
+        public Builder() {
+            this. _references = new ArrayList<>();
+        }
+
+        public Builder entityName(String entityName) {
+            _entityName = entityName;
+            return this;
+        }
+
+        public Builder type(EntityType type) {
+            _type = type;
+            return this;
+        }
+
+        public Builder references(List<EntityValue> entityValues) {
+            _references = entityValues;
+            return this;
+        }
+
+        public Builder addReference(EntityValue entityValue) {
+            _references.add(entityValue);
+            return this;
+        }
+
+        public Entity build(){
+            Entity entity = new Entity();
+            entity.setEntityName(_entityName);
+            entity.setType(_type);
+            entity.setReferences(_references);
+
+            return entity;
+        }
+
     }
 }
