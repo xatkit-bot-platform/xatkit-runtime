b581883a458d6b012079cd35d0f0c0e5de877031
Hamza <hamza.eddouibi@gmail.com>
2020-09-10 00:37:09 +0200

    Add reverse entity map

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/mapper/NlpjsEntityReferenceMapper.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/mapper/NlpjsEntityReferenceMapper.java
@@ -3,16 +3,25 @@ package com.xatkit.core.recognition.nlpjs.mapper;
 import com.xatkit.core.recognition.EntityMapper;
 import com.xatkit.intent.CustomEntityDefinition;
 
+import lombok.NonNull;
+
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.xatkit.intent.EntityType.*;
 
-public class NlpjsSlotMapper extends EntityMapper {
+public class NlpjsEntityReferenceMapper extends EntityMapper {
 
-    public NlpjsSlotMapper() {
+    private Map<String, List<String>> reversedEntities;
+
+    public NlpjsEntityReferenceMapper() {
         super();
         this.registerEntities();
         this.setFallbackEntityMapping("any");
+        this.reversedEntities = this.reverseEntityTypes();
     }
 
     private void registerEntities() {
@@ -39,5 +48,26 @@ public class NlpjsSlotMapper extends EntityMapper {
     protected String getMappingForCustomEntity(CustomEntityDefinition customEntityDefinition) {
         return customEntityDefinition.getName();
     }
+
+    private Map<String, List<String>> reverseEntityTypes() {
+        Map<String, List<String>> reversedEntities = new HashMap<>();
+        for (Map.Entry<String, String> entityEntry: entities.entrySet()) {
+            if (reversedEntities.containsKey(entityEntry.getValue())) {
+                List<String> entityTypes = reversedEntities.get(entityEntry.getKey());
+                entityTypes.add(entityEntry.getKey());
+            }
+            else {
+                List<String> entityTypes = new ArrayList<>();
+                entityTypes.add(entityEntry.getKey());
+                reversedEntities.put(entityEntry.getValue(),entityTypes);
+            }
+        }
+        return reversedEntities;
+    }
+
+    public List<String> getReversedEntity(@NonNull String nlpjsEntityType) {
+        return reversedEntities.get(nlpjsEntityType);
+    }
+
 
 }
