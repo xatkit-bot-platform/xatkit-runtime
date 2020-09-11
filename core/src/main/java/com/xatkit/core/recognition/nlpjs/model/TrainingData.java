b52a0355e790610c43a6027d2be19214f6e70ce7
Hamza <hamza.eddouibi@gmail.com>
2020-09-11 12:41:57 +0200

    Add TrainingData builder

--- core/src/main/java/com/xatkit/core/recognition/nlpjs/model/TrainingData.java
+++ core/src/main/java/com/xatkit/core/recognition/nlpjs/model/TrainingData.java
@@ -1,26 +1,69 @@
 package com.xatkit.core.recognition.nlpjs.model;
 
+import com.xatkit.intent.IntentDefinition;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static fr.inria.atlanmod.commons.Preconditions.checkNotNull;
+import static fr.inria.atlanmod.commons.Preconditions.checkArgument;
+
 @Data
 public class TrainingData {
 
     private AgentConfig config;
+
     private List<Intent> intents;
+
     private List<Entity> entities;
 
-
-    public TrainingData() {
-        intents = new ArrayList<>();
-        entities = new ArrayList<>();
+    private TrainingData() {}
+    public static Builder newBuilder() {
+        return new Builder();
     }
 
-    public TrainingData(AgentConfig config, List<Intent> intents, List<Entity> entities) {
-        this.config = config;
-        this.intents = intents;
-        this.entities = entities;
+    public static class Builder {
+
+        private AgentConfig _config;
+
+        private List<Intent> _intents;
+
+        private List<Entity> _entities;
+
+        public Builder config(AgentConfig _config) {
+            this._config = _config;
+            return this;
+        }
+
+        public Builder intents(List<Intent> _intents) {
+            this._intents = _intents;
+            return this;
+        }
+
+        public Builder entities(List<Entity> _entities) {
+            this._entities = _entities;
+            return this;
+        }
+
+        public TrainingData build() {
+            checkNotNull(_config, "Cannot train the bot with the provided configuration %s",
+                    AgentConfig.class.getSimpleName());
+            checkNotNull(_intents, "Cannot train the bot with the provided intents %s",
+                    Intent.class.getSimpleName());
+            checkArgument(!_intents.isEmpty(), "Cannot train the bot with an empty intent list %s",
+                    Intent.class.getSimpleName());
+            TrainingData trainingData = new TrainingData();
+            trainingData.setConfig(_config);
+            trainingData.setEntities(_entities);
+            trainingData.setIntents(_intents);
+            return trainingData;
+        }
+
+
+
     }
+
+
+
 }
