b624431cf02d4d1a797494018e9361ac9040f550
Gwendal Daniel <gwendal.daniel@gmail.com>
2021-01-19 15:32:32 +0100

    fix NlpjsClient string format and optimize imports

--- src/main/java/com/xatkit/core/recognition/nlpjs/NlpjsClient.java
+++ src/main/java/com/xatkit/core/recognition/nlpjs/NlpjsClient.java
@@ -4,7 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.xatkit.core.recognition.IntentRecognitionProviderException;
 import com.xatkit.core.recognition.nlpjs.adapter.ExtractedEntityDeserializer;
-import com.xatkit.core.recognition.nlpjs.model.*;
+import com.xatkit.core.recognition.nlpjs.model.Agent;
+import com.xatkit.core.recognition.nlpjs.model.AgentInit;
+import com.xatkit.core.recognition.nlpjs.model.ErrorBody;
+import com.xatkit.core.recognition.nlpjs.model.ExtractedEntity;
+import com.xatkit.core.recognition.nlpjs.model.RecognitionResult;
+import com.xatkit.core.recognition.nlpjs.model.TrainingData;
+import com.xatkit.core.recognition.nlpjs.model.UserMessage;
 import fr.inria.atlanmod.commons.log.Log;
 import lombok.NonNull;
 import okhttp3.OkHttpClient;
@@ -53,8 +59,8 @@ public class NlpjsClient {
         Response<Agent> agentResponse = nlpjsApi.getAgentInfo(agentId).execute();
         if (!agentResponse.isSuccessful()) {
             ErrorBody errorBody = gson.fromJson(agentResponse.errorBody().string(), ErrorBody.class);
-            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} " +
-                            "on {1}. The API responded with the status {2} and the error message \"{3}\" ",
+            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} on {1}. The API responded "
+                            + "with the status {2} and the error message \"{3}\" ",
                     nlpjsApi.getAgentInfo(agentId).request().method(),
                     nlpjsApi.getAgentInfo(agentId).request().url(), agentResponse.code(), errorBody.getMessage());
             throw new IntentRecognitionProviderException(errorMessage);
@@ -67,8 +73,8 @@ public class NlpjsClient {
         Response<Void> response = nlpjsApi.createAgent(agentInit).execute();
         if (!response.isSuccessful()) {
             ErrorBody errorBody = gson.fromJson(response.errorBody().string(), ErrorBody.class);
-            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} " +
-                            "on {1}. The API responded with the status {2} and the error message \"{3}\" ",
+            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} on {1}. The API responded "
+                            + "with the status {2} and the error message \"{3}\" ",
                     nlpjsApi.createAgent(agentInit).request().method(),
                     nlpjsApi.createAgent(agentInit).request().url(), response.code(), errorBody.getMessage());
             throw new IntentRecognitionProviderException(errorMessage);
@@ -76,25 +82,28 @@ public class NlpjsClient {
         return true;
     }
 
-    public boolean trainAgent(String agentId, TrainingData trainingData) throws IOException, IntentRecognitionProviderException {
+    public boolean trainAgent(String agentId, TrainingData trainingData) throws IOException,
+            IntentRecognitionProviderException {
         Response<Void> response = nlpjsApi.trainAgent(agentId, trainingData).execute();
         if (!response.isSuccessful()) {
             ErrorBody errorBody = gson.fromJson(response.errorBody().string(), ErrorBody.class);
-            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} " +
-                            "on {1}. The API responded with the status {2} and the error message: {3} ",
+            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} on {1}. The API responded "
+                            + "with the status {2} and the error message: {3} ",
                     nlpjsApi.trainAgent(agentId, trainingData).request().method(),
-                    nlpjsApi.trainAgent(agentId, trainingData).request().url(), response.code(), errorBody.getMessage());
+                    nlpjsApi.trainAgent(agentId, trainingData).request().url(), response.code(),
+                    errorBody.getMessage());
             throw new IntentRecognitionProviderException(errorMessage);
         }
         return true;
     }
 
-    public RecognitionResult getIntent(@NonNull String agentId, @NonNull UserMessage userMessage) throws IOException, IntentRecognitionProviderException {
+    public RecognitionResult getIntent(@NonNull String agentId, @NonNull UserMessage userMessage) throws IOException,
+            IntentRecognitionProviderException {
         Response<RecognitionResult> response = nlpjsApi.getIntent(agentId, userMessage).execute();
         if (!response.isSuccessful()) {
             ErrorBody errorBody = gson.fromJson(response.errorBody().string(), ErrorBody.class);
-            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} " +
-                            "on {1}. The API responded with the status {2} and the error code {3} ",
+            String errorMessage = MessageFormat.format("Unsuccessful REST operation {0} on {1}. The API responded "
+                            + "with the status {2} and the error code {3} ",
                     nlpjsApi.getIntent(agentId, userMessage).request().method(),
                     nlpjsApi.getIntent(agentId, userMessage).request().url(), response.code(), errorBody.getMessage());
             throw new IntentRecognitionProviderException(errorMessage);
