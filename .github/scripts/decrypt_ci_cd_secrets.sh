a8e2bf85e8e191396fcf5539b10256c62492d1ed
Gwendal Daniel <gwendal.daniel@gmail.com>
2021-03-16 01:47:42 +0100

    add CI/CD secrets in GA workflow

---
+++ .github/scripts/decrypt_ci_cd_secrets.sh
@@ -0,0 +1,15 @@
+# This is required because Xatkit is not yet on Maven Central or similar
+
+# Print a message
+e() {
+    echo -e "$1"
+}
+
+main() {
+	
+    e "Decrypting CI/CD secrets"
+    gpg --quiet --batch --yes --decrypt --passphrase="$CI_CD_KEY" --output ./src/test/resources/xatkit-secrets.zip xatkit-secrets.zip.gpg
+    unzip ./src/test/resources/xatkit-secrets.zip -d ./src/test/resources/
+}
+
+main
